d52aee1a75716c069dd83a13544e24ee533be1b1
mandeep <singh.mr.1996@gmail.com>
2021-07-01 19:41:13 +0530

    append in linklist

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+public class Main 
+{
+	public static void main(String[] args) {
+		System.out.println("Welcome to DataStructure Program");
+		
+		linkList List = new linkList();
+		
+		 List.insert(56);
+		 List.insert(30);
+		 List.insert(70);
+		
+		List.print();
+		
+	
+	}
+}
